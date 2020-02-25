a9d28d8996aa82380c356430f0596a0d1fec2f12
ShivaniMehrotra4 <shivanimehrotra.sms@gmail.com>
2020-02-25 16:48:07 +0530

    removed print

--- src/main/scala/com/edu/AreaCalculator.scala
+++ src/main/scala/com/edu/AreaCalculator.scala
@@ -4,19 +4,14 @@ object Constants {
   val length = 4
   val breadth = 6
 }
+
 object AreaCalculator extends App {
   val rectangle: Rectangle = new Rectangle
   val square: Square = new Square
   val rectangleAsParent: Rectangle = new Square
 
-
   val areaOfRectangle = rectangle.calculateArea(Constants.length, Constants.breadth)
   val areaOfSquare = square.calculateArea(Constants.length, Constants.breadth)
   val testResult = rectangleAsParent.calculateArea(Constants.length, Constants.breadth)
-
-  println(areaOfRectangle)
-  println(areaOfSquare)
-  println(testResult)
-
 
 }
